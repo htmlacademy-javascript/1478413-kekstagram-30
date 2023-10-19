3faee0eef556faac78295875f83e88aa497b739c
romkafomin <fomin.romka@yandex.ru>
2023-10-19 20:07:37 +0300

    4 домашка, окончательный вариант

--- js/main.js
+++ js/main.js
@@ -1,58 +1,51 @@
-//функция получения рандомного числа в диапазоне
-const getRandomInteger = (a, b) => {
+const namesList = [
+  'Иван',
+  'Дмитрий',
+  'Ксения',
+  'Виктор',
+  'Екатерина',
+  'Анастасия',
+  'Антон',
+];
+
+const commentList = ['Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const descriptionList = [
+  'описание 1',
+  'описание 2',
+  'описание 3',
+  'описание 4',
+  'описание 5',
+  'описание 6',
+  'описание 7',
+  'описание 8',
+  'описание 9',
+];
+
+/*
+ * функция для возврата рандомного значения из массива
+ * @param {Array} inputArray — изначальный массив
+ * @return {*} — случайный элемент массива
+ */
+const getRandomInteger = function (a, b) {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 };
 
-//функция для возврата рандомного описания
-const generateRandomComment = function (){
-  const commentList = ['Всё отлично!',
-    'В целом всё неплохо. Но не всё.',
-    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',];
-
-  const randomComment = commentList[Math.round(Math.random() * (commentList.length - 1))];
-  return randomComment;
-};
-
-//функция возвращает рандомное описание
-const getDescription = function () {
-  const descriptionList = [
-    'описание 1','описание 2','описание 3','описание 4','описание 5','описание 6','описание 7','описание 8','описание 9',
-  ];
-  const randomDescription = descriptionList[Math.round(Math.random() * (descriptionList.length - 1))];
-  return randomDescription();
-};
-
-//функция для возврата рандомного имени
-const generateRandomName = function (){
-  const namesList = [
-    'Иван',
-    'Дмитрий',
-    'Ксения',
-    'Виктор',
-    'Екатерина',
-    'Анастасия',
-    'Антон',
-  ];
-
-  const randomName = namesList [Math.round(Math.random() * (namesList.length - 1))];
-  return randomName;
-};
-
-//функция для генерации одного рандомного уникального числа в диапазоне
-const generateRandomLikes = function () {
-  const min = 15;
-  const max = 200;
-  return Math.round(Math.random() * (max - min) + min);
-};
-
-//функция для возврата уникального числа по возрастанию
-const createIdGenerator = function () {
+/*
+ * функция для возврата чисел по возрастанию
+ * @param - без параметров
+ * @return {*} — число по возрастанию от единицы
+ */
+const createId = function () {
   let lastGeneratedId = 0;
 
   return function () {
@@ -61,25 +54,37 @@ const createIdGenerator = function () {
   };
 };
 
-//функция для создания массива описаний
+const idCreator = createId();
+const urlCreator = createId();
+
+/*
+ * функция для возврата чисел по возрастанию
+ * @param - без параметров
+ * @return {*} — объект с информацией о пользователе
+ */
 const generateCommentsList = function () {
   return {
-    id: createIdGenerator,
-    avatar: `img/avatar${getRandomInteger(1,6)}$.svg`,
-    message: generateRandomComment,
-    name: generateRandomName,
+    id: getRandomInteger(1,10000),
+    avatar: `img/avatar${getRandomInteger(1,6)}.svg`,
+    message: commentList[getRandomInteger(0,commentList.length - 1)],
+    name: namesList[getRandomInteger(0,namesList.length - 1)],
   };
 };
 
-//функция для создания массива объектов
-const generatePhotoDescription = function () {
+/*
+ * функция для возврата чисел по возрастанию
+ * @param - без параметров
+ * @return {*} — объект с информацией о опубликованной фотографии
+ */
+const generatePhotoData = function () {
   return {
-    id : createIdGenerator,
-    url :  `photos/${createIdGenerator}$.jpg`,
-    decription : getDescription,
-    likes : generateRandomLikes,
+    id : idCreator(),
+    url :  `photos/${urlCreator()}.jpg`,
+    decription : descriptionList[getRandomInteger(0,descriptionList.length - 1)],
+    likes : getRandomInteger(15,200),
     comments: Array.from({length: getRandomInteger(1,30)} , generateCommentsList),
   };
 };
 
-const generateArrayPhotos = Array.from({length: 25}, generatePhotoDescription);
+const generateArrayPhotos = Array.from({length: 25}, generatePhotoData);
+
